d9cca766dedb25a20d4410fba77d8952e8f92298
Carlos Eduardo Lescano Morisson <carlos.morisson@sesims.com.br>
2024-08-15 15:42:26 -0400

    ambient

---
+++ Assets/RippleEffectAssets/RippleCollider.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RippleCollider : MonoBehaviour
+{
+    public ParticleSystem ripple;
+    private bool inWater;
+    // Start is called before the first frame update yup
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ripple.transform.position = transform.position;
+        Shader.SetGlobalVector("_Player", transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
+        }
+    }
+}
