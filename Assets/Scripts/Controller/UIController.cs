a108a9ddae13e9b38c9f123f5ad9c431abe59feb
Carlos Eduardo Lescano Morisson <carlos.morisson@sesims.com.br>
2024-12-13 15:27:14 -0400

    start ui

--- Assets/Scripts/Controller/UIController.cs
+++ Assets/Scripts/Controller/UIController.cs
@@ -7,4 +7,30 @@ using DG.Tweening;
 public class UIController : MonoBehaviour
 {
     public static UIController instance;
+    [Header("Start Canva")]
+    public GameObject StartCanva;
+    //Elements
+    [SerializeField]
+    private Image initialImage;
+    [SerializeField]
+    private Image kissImage;
+    [Header("Hud Canva")]
+    public GameObject HudCanva;
+    //Elements
+    [SerializeField]
+    private Image hudImage;
+    [SerializeField]
+    private TextMeshProUGUI playerScore, enemyScore;
+    [Header("Final Canva")]
+    public GameObject FinalCanva;
+    //Elements
+    [SerializeField]
+    private TextMeshProUGUI playerFinalScore, enemyFinalScore;
+    [SerializeField]
+    private Image finalImage;
+    private void Start()
+    {
+        instance = this;
+    }
+
 }
